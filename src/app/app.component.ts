06d62afac101fd1df3cdd9a57a9072098c6ccc5d
Pablo García Sánchez <fergunet@gmail.com>
2018-01-16 13:05:58 +0100

    Modificaciones para el ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,44 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [
+  //   require('bootstrap/dist/css/bootstrap')
+  // ]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Select an option :';
+    isSideNavVisible = true;
+
+    showHideSideClicked() {
+        if (this.isSideNavVisible) {
+            document.getElementById('main')
+                .style.marginLeft = "0px";
+            document.getElementById('mySidenav')
+                .style.width = "0px";
+            this.isSideNavVisible = false;
+        } else {
+            document.getElementById('main')
+                .style.marginLeft = "250px";
+            document.getElementById('mySidenav')
+                .style.width = "250px";
+            this.isSideNavVisible = true;
+        }
+    }
+
+buttonClickedDetail() {
+    document.getElementById('myRightScreen')
+        .style.transform = "translateX(0%)";
+    document.getElementById('main')
+        .style.transform = "translateX(-100%)";
+    //this.showHideSideClicked();
+    }
+
+
+closeClicked() {
+    document.getElementById('myRightScreen')
+        .style.transform = "translateX(100%)";
+    document.getElementById('main')
+        .style.transform = "translateX(0%)";
+}
+
 }
